ea7dab7a0537d66e161c957e64200a5401cccf53
Cyrus Arora <cyrus.s.arora@gmail.com>
2019-01-27 04:13:46 -0800

    button work

--- src/LandingPage/LandingPage.js
+++ src/LandingPage/LandingPage.js
@@ -1,18 +1,23 @@
-import React, { Component } from 'react';
-import './LandingPage.css';
-import Button from '../Button/Button.js';
-import Logo from '../Logo/Logo.js';
+import React, { Component } from "react";
+import "./LandingPage.css";
+import Button from "../Button/Button.js";
+import Logo from "../Logo/Logo.js";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 class LandingPage extends Component {
   render() {
     return (
-      <div className = "contents">
+      <div className="contents">
         <center>
-        <Logo />
-        <div className = "buttons">
-          <Button state ='primary' text='Join a Room' />
-          <Button state = 'secondary' text='Create a Room'/>
-        </div>
+          <Logo />
+          <div className="buttons">
+            <Link to="/JoinRoom">
+              <Button state="primary" text="Join a Room" />
+            </Link>
+            <Link to="/CreateRoom">
+              <Button state="secondary" text="Create a Room" />
+            </Link>
+          </div>
         </center>
       </div>
     );
