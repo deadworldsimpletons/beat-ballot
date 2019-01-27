f5fef1581a49bbf758000f9be6a23cd7b0d9b1fa
madigan-99 <fymadigan@outlook.com>
2019-01-27 00:04:32 -0800

    song card update

--- src/SongCard/SongCard.js
+++ src/SongCard/SongCard.js
@@ -5,23 +5,29 @@ import './SongCard.css';
 class SongCard extends Component {
   render() {
     return (
+        <center>
         <div className = "card">
-          <div className = "cover">
-            <img src = {this.props.albumCover} />
-          </div>
-          <div className = "songInfo">
-            <p> {this.props.artist}</p>
-            <p> &mdash; </p>
-            <p>{this.props.songName}</p>
+
+          <div className = "contain">
+            <div className = "cover">
+              <img src = {this.props.albumCover} />
+            </div>
+
+            <div className = "songInfo">
+              <p> <span className='artist'> {this.props.artist} </span>
+              <span> &mdash; </span>
+              <span  className='songTitle'> {this.props.songName} </span> </p>
+            </div>
           </div>
 
           <div className = "votes">
-            <p>{this.props.upVote}</p>
-            <p> &mdash; </p>
-            <p>{this.props.downVote}</p>
+            <p> <span  className='upVote'> {this.props.upVote} </span>
+            <span> &mdash; </span>
+            <span  className='downVote'> {this.props.downVote} </span> </p>
           </div>
 
         </div>
+        </center>
     );
   }
 }
