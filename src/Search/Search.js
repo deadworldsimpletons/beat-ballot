c288d85cda7b33c07597fcc075e378f926c8a2b4
Cyrus Arora <cyrus.s.arora@gmail.com>
2019-01-27 04:56:30 -0800

    fixed search

--- src/Search/Search.js
+++ src/Search/Search.js
@@ -1,34 +1,50 @@
+/*global lib*/
 import React, { Component } from "react";
 import config from "../config.js";
 import "./Search.css";
 import SongCardList from "../SongCardList/SongCardList.js";
+import Button from "../Button/Button.js";
 
 class Search extends Component {
   state = {
     q: "",
-    results: []
+    results: false
   };
 
   constructor(props) {
     super(props);
     this.doSearch = this.doSearch.bind(this);
+    this.updateQuery = this.updateQuery.bind(this);
   }
 
   doSearch(e) {
-    const q = e.target.value;
-    this.setState({ q });
-    config.lib.search({ q }, (err, res) => {
+    lib.nwhacks19.api["@dev"].search({ q: this.state.q }, (err, res) => {
       if (err) throw err;
       this.setState({ results: res });
     });
+  }
+
+  updateQuery(e) {
+    this.setState({ q: e.target.value });
   }
 
   render() {
     return (
       <div>
         <center>
-        <input placeholder="Search..." onChange={this.doSearch} />
-        <SongCardList songs={this.state.results} onClick={this.props.onClick} />
+          {this.state.results ? (
+            <div>
+              <SongCardList
+                songs={this.state.results}
+                onClick={this.props.onClick}
+              />
+            </div>
+          ) : (
+            <div>
+              <input placeholder="Search..." onChange={this.updateQuery} />
+              <Button state="secondary" text="Search" onClick={this.doSearch} />
+            </div>
+          )}
         </center>
       </div>
     );
