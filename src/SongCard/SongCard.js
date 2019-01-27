b6005cc37876875a03f06a7c2c437c807216b387
madigan-99 <fymadigan@outlook.com>
2019-01-26 22:45:00 -0800

    song card updates and logo

--- src/SongCard/SongCard.js
+++ src/SongCard/SongCard.js
@@ -1,18 +1,16 @@
 import React, { Component } from 'react';
 import './SongCard.css';
-import albumArt from '../tempImages/albumCover.png'
+
 
 class SongCard extends Component {
   render() {
     return (
         <div className = "card">
           <div className = "cover">
-            {this.props.albumCover}
-            <img src = {albumArt} />
+            <img src = {this.props.albumCover} />
           </div>
-
-          <div className = "songInfo">{this.props.songName} <p>POWER </p> -- <p>KANYE WEST</p></div>
-          <div className = "votes">{this.props.upVote} {this.props.downVote}<p> 5 - 3</p></div>
+          <div className = "songInfo"><p>{this.props.artist}</p> <p> &mdash; </p> {this.props.songName}</div>
+          <div className = "votes">{this.props.upVote} <p> &mdash; </p> {this.props.downVote}</div>
         </div>
     );
   }
